1da07f5f8ddf1a2fc20bac12b58fda8cb23986be
tausifcoder1111 <tausifcoderr@gmail.com>
2026-04-03 12:48:35 +0530

    core java

--- Implementation/Helper.java
+++
@@ -1,5 +0,0 @@
-public class Helper {
-    public static void getString(){
-        System.out.println("Hello String");
-    }
-}
--- Main.java
+++
@@ -1,7 +0,0 @@
-import Implementation.Helper
-public class Main{
-    public static  void main(String[] args){
-        System.out.println("Hello");
-        Helper.getString();
-    }
-}
