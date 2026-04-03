abeddd57ee516699183845ed2ee030195318b439
tausifcoder1111 <tausifcoderr@gmail.com>
2026-04-03 11:48:44 +0530

    Input & Output

---
+++ 02-Core-Java/src/IntroTopics/HelloWorld.java
@@ -0,0 +1,12 @@
+package IntroTopics;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("1. Println");
+        System.out.println("2. Println");
+        System.out.print("1. Print");
+        System.out.print("2. Print");
+//        System.out.printf("1. Print");
+//        System.out.printf("2. Print");
+    }
+}
